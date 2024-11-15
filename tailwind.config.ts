7d4c0e77b8d2c3b6826ec7bc8dd3ee669cab130c
Gijo John <gijojohn@Gijos-MacBook-Pro.local>
2024-11-15 21:52:32 +0530

    modifying grid and changing tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,11 @@
 import type { Config } from "tailwindcss"
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  important: true,
+  mode: 'jit',
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {
-      backgroundColor: {
-        'page': '#f8fafc',
-      },
-      colors: {
-        primary: '#2563eb', // blue-600
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
